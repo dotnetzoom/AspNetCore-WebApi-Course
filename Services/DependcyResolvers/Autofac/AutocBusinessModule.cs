2e4704e462035b21c6ef717eb345746bec8a8b98
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-09-12 11:32:24 +0300

    Added Autofac register class to services layer.

---
+++ Services/DependcyResolvers/Autofac/AutocBusinessModule.cs
@@ -0,0 +1,43 @@
+﻿using Autofac;
+using Common;
+using Data;
+using Data.Repositories;
+using Entities;
+using Microsoft.AspNetCore.Http;
+
+namespace Services.DependcyResolvers.Autofac
+{
+    public class AutocBusinessModule : Module
+    {
+        protected override void Load(ContainerBuilder containerBuilder)
+        {
+            containerBuilder.RegisterGeneric(typeof(Repository<>)).As(typeof(IRepository<>)).InstancePerLifetimeScope();
+
+            var commonAssembly = typeof(SiteSettings).Assembly;
+            var entitiesAssembly = typeof(IEntity).Assembly;
+            var dataAssembly = typeof(ApplicationDbContext).Assembly;
+            var servicesAssembly = typeof(JwtService).Assembly;
+
+            containerBuilder.RegisterAssemblyTypes(commonAssembly, entitiesAssembly, dataAssembly, servicesAssembly)
+                .AssignableTo<IScopedDependency>()
+                .AsImplementedInterfaces()
+                .InstancePerLifetimeScope();
+
+            containerBuilder.RegisterAssemblyTypes(commonAssembly, entitiesAssembly, dataAssembly, servicesAssembly)
+                .AssignableTo<ITransientDependency>()
+                .AsImplementedInterfaces()
+                .InstancePerDependency();
+
+            containerBuilder.RegisterAssemblyTypes(commonAssembly, entitiesAssembly, dataAssembly, servicesAssembly)
+                .AssignableTo<ISingletonDependency>()
+                .AsImplementedInterfaces()
+                .SingleInstance();
+            //
+            containerBuilder.RegisterType<HttpContextAccessor>().As<IHttpContextAccessor>();
+
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+
+            containerBuilder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces().SingleInstance();
+        }
+    }
+}
